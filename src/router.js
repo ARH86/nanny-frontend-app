4a98d885c076fa0671846b6fdcaac53f0664da03
Adam <ahanson845@gmail.com>
2018-10-17 21:32:56 -0500

    created frontend app

--- src/router.js
+++ src/router.js
@@ -12,8 +12,14 @@ export default new Router({
       component: Home
     },
     {
+      path: '/feed',
+      name: 'feed',
+      
       path: '/about',
       name: 'about',
+    
+
+    
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
+<style>
+  body {
+    background-color: lightblue;
+    
+
+  }
+
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  var axios = require ('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "The Caretaker App"
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/jobs")
+    .then(function(response) { 
+      this.jobs = response.data;
+    }.bind(this));
+
+  },
+  methods: {},
+  computed: {}
+};
 </script>
